b40833fa37da1882dd9eced2324d239300358594
JakeTacogdoy <jaketacogdoy88@gmail.com>
2024-01-23 15:26:22 +0800

    changed login

--- config/variables.php
+++ config/variables.php
@@ -3,7 +3,7 @@
   return [
     "creatorName" => "ThemeSelection",
     "creatorUrl" => "https://themeselection.com",
-    "templateName" => "Sneat",
+    "templateName" => "Alumni Tracer",
     "templateSuffix" => "Bootstrap 5 HTML Laravel Free Admin Template",
     "templateVersion" => "1.1.0",
     "templateFree" => true,
