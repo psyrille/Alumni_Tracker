d6ba793c36b8f331d23255a7dffef7da5e10d380
JakeTacogdoy <jaketacogdoy88@gmail.com>
2024-02-06 09:36:28 +0800

    admin dashboard

--- app/Http/Controllers/admin/AdminDashboard.php
+++ app/Http/Controllers/admin/AdminDashboard.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminDashboard extends Controller
 {
   public function index()
   {
-    return view('content.admin.dashboard');
+    $users = User::all();
+    return view('content.admin.dashboard',compact('users'));
   }
 }
--- resources/views/content/admin/dashboard.blade.php
+++ resources/views/content/admin/dashboard.blade.php
@@ -15,5 +15,44 @@
 @endsection
 
 @section('content')
-    
+
+    <div class="card">
+        <h5 class="card-header">Users</h5>
+        <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-sm">
+            
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Student No.</th>
+                    <th>Campus</th>
+                    <th>Course</th>
+                    <th>Year Graduated</th>
+                    <th>Facebook</th>
+                    <th>Contact No.</th>
+                    <th>Address</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                <tr>
+                    <td class="text-nowrap">{{$user->firstName}} {{$user->lastName}} </td>
+                    <td class="text-nowrap">{{$user->studentNo}}</td>
+                    <td class="text-nowrap">{{$user->campus}}</td>
+                    <td class="text-nowrap">{{$user->course}}</td>
+                    <td class="text-nowrap">{{$user->yearGrad}}</td>
+                    <td class="text-nowrap">{{$user->facebook}}</td>
+                    <td class="text-nowrap">{{$user->contactNo}}</td>
+                    <td class="text-nowrap">{{$user->address}}</td>    
+                    <td class="text-nowrap">{{$user->email}}</td>                
+                </tr>
+                @endforeach
+            </tbody>
+          </table>
+            </div>
+        </div>
+    </div>
+
 @endsection
