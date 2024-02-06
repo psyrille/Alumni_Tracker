ac4112de964ee03e1cbf9f09b0c10f8b1ae4c72b
JakeTacogdoy <jaketacogdoy88@gmail.com>
2024-02-06 09:47:08 +0800

    mail

--- resources/views/mail/index.blade.php
+++ resources/views/mail/index.blade.php
@@ -3,8 +3,8 @@ $home = URL::to('/');
 ?>
 
 <div class="container-fluid">
-    <h2><span class="text-success">CONGRATULATIONS!</span></h2>
-    <span>YOU HAVE SUCCESSFULLY REGISTERED TO THE SLSU ONLINE ADMISSION!</span>
+    <h2><span class="text-success">Account Disapproved.</span></h2>
+    
 
     <span>
         <br><br>
@@ -12,12 +12,5 @@ $home = URL::to('/');
         MESSAGE: <b>{{ mb_strtoupper($email_data['Message']) }}</b>
     </span>
     <br><br>
-    <div>NEXT STEPS TO DO:
-        <ol>
-            <li class="li">Login to your account. <a target = "_blank" href="{{ $home }}">CLICK HERE</a></li>
-            <li class="li">Complete the registration process by verifying your email and contact number.</li>
-            <li class="li">Upload the required documents.</li>
-            <li class="li">Upload a copy of your 2x2 picture with white background and nametag</li>
-        </ol>
-    </div>
+    
 </div>
